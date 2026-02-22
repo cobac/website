42c85fba5f3407e2c12c0bfb2fc9e6fb0b4ea772
coba <coba@cobac.eu>
2026-02-22 13:19:13 +0100

    s/posted/last updated

--- lisp/build.el
+++ lisp/build.el
@@ -93,7 +93,7 @@ Modify this function if you want to change a posts headline."
    (when (org-static-blog-get-date
           post-filename)
      (concat
-      "<div class=\"post-date\">\n" "~ Posted on: "
+      "<div class=\"post-date\">\n" "~ Last updated on: "
       (format-time-string
        (org-static-blog-gettext
         'date-format)
